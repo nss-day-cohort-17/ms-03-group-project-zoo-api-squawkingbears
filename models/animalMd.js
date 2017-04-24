b6feaa1417f484d7f62685316bf223cae5ae57c2
j-west <westja89@gmail.com>
2017-04-24 13:50:27 -0500

    Add three methods with logic to add, edit, and delete and animal

--- models/animalMd.js
+++ models/animalMd.js
@@ -12,12 +12,27 @@ const Animal = bookshelf.Model.extend({
   getAll: function() {
     return this.forge()
     .fetchAll()
-    .then(rows => rows)
+    .then(animals => animals)
     .catch(error => error)
   },
-  getOneAnimal: function() {
+  getOneAnimal: function(id) {
     return this.forge({id})
     .fetch()
+    .then(animal => animal)
+    .catch(error => error)
+  },
+  addAnimal: function(newAnimal) {
+
+  },
+  deleteAnimal: function(id) {
+    return this.forge({id})
+    .destroy()
+    .then(animal => animal)
+    .catch(error => error)
+  },
+  editAnimal: function(id, animalEdits) {
+    return this.where({id})
+    .save(animalEdits, {method: 'update'})
     .then(animal => animal)
     .catch(error => error)
   }
