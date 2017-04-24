028510ed37690477d59e8087d1fdd733397008c9
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-24 15:21:09 -0500

    created get routes

---
+++ routes/animals.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { Router } = require('express');
+
+const router = Router();
+
+const {  } = require('../controllers/animalCtrl');
+const {  } = require('../controllers/trainerCtrl');
+const {  } = require('../controllers/trickCtrl');
+const {  } = require('../controllers/zoneCtrl');
+const {  } = require('../controllers/zookeeperCtrl');
+
+
+router.get('/animals', )
+router.get('/trainers', )
+router.get('/tricks', )
+router.get('/zones', )
+router.get('/zookeepers', )
+
+
+module.exports = router;
