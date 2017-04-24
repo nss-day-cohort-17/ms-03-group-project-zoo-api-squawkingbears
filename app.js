f026204fcfb308c69cac06e4486d1821aae5b3d9
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-24 11:32:05 -0500

    removed path from reqs

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 // pull in required modules and routes
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes/');
 
