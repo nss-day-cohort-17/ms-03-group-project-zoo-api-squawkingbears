46ec2c041bb4672b2f5f61b13f09c1644d34624e
j-west <westja89@gmail.com>
2017-04-24 15:17:45 -0500

    Change names of functions in animalCtrl

--- controllers/animalCtrl.js
+++ controllers/animalCtrl.js
@@ -3,31 +3,31 @@
 const { bookshelf } = require('../db/database')
 const Animal = require('../models/animalMd')
 
-module.exports.getAnimals = (req, res, next) => {
+module.exports.getAll = (req, res, next) => {
   Animal.getAll()
   .then(animals => res.status(200).json(animals))
   .catch(error => next(error))
 }
 
-module.exports.getAnimal = ({body: {id}}, res, next) => {
+module.exports.getOneAnimal = ({body: {id}}, res, next) => {
   Animal.getOneAnimal(id)
   .then(animal => res.status(200).json(animal))
   .catch(error => next(error))
 }
 
-module.exports.newAnimal = ({body}, res, next) => {
+module.exports.addAnimal = ({body}, res, next) => {
   Animal.addAnimal(body)
   .then(animal => res.status(200).json(animal))
   .catch(error => next(error))
 }
 
-module.exports.removeAnimal = ({body: {id}}, res, next) => {
+module.exports.deleteAnimal = ({body: {id}}, res, next) => {
   Animal.deleteAnimal(id)
   .then(animal => res.status(200).json(animal))
   .catch(error => next(error))
 }
 
-module.exports.animalEdit = ({body}, res, next) => {
+module.exports.editAnimal = ({body}, res, next) => {
   Animal.editAnimal
   .then(animal => res.status(200).json(animal))
   .catch(error => next(error))
