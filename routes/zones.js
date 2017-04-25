1abe10ca7fabee3ef291beec7603fa332833392f
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-25 09:54:54 -0500

    created routes for zone

---
+++ routes/zones.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { Router } = require('express');
+
+const router = Router();
+
+const { getAll, getOneZone, addZone, deleteZone, editZone } = require('../controllers/zoneCtrl');
+
+
+router.get('/zones', getAll);
+router.get('/zones/:id', getOneZone);
+router.post('/zones', addZone);
+router.delete('/zones/:id', deleteZone);
+router.patch('/zones', editAnimal);
+
+module.exports = router;
