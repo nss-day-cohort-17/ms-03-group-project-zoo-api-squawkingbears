82e6d20f7db1f91b8abd29b0fd44af5c42c025b9
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-25 13:24:02 -0500

    stashing

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const { Router } = require('express');
-
 const router = Router();
 
 router.use(require('./animals'))
 // router.use(require('./trainers'))
 // router.use(require('./tricks'))
-router.use(require('./zones'))
+// router.use(require('./zones'))
 // router.use(require('./zookeepers'))
 
 module.exports = router;
