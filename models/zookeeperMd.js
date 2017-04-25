964022bb544fc7fe38f3bf64b08c3ec18b572666
j-west <westja89@gmail.com>
2017-04-25 09:25:19 -0500

    Add zookeeper model with five methods.  getAll, getOne, add, delete, and edit

---
+++ models/zookeeperMd.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const { bookshelf } = require('../db/database')
+require('./zoneMd')
+
+const ZooKeeper = bookshelf.Model.extend({
+  tableName: zookeepers,
+  zone: function() { return this.hasOne('Zone')}
+}, {
+  getAll: function() {
+    return this.forge()
+    .fetchAll()
+    .then(zooKeepers => zooKeepers)
+    .catch(error => error)
+  },
+  getOneZooKeeper: function(id) {
+    return this.forge({id})
+    .fetch()
+    .then(zooKeeper => zooKeeper)
+    .catch(error => error)
+  },
+  addZooKeeper: function(newZooKeeper) {
+    return this.forge(newZooKeeper)
+    .save()
+    .then(zooKeeper => zooKeeper)
+    .catch(error => error)
+  },
+  deleteZooKeeper: function(id) {
+    return this.forge({id})
+    .destroy()
+    .then(zooKeeper => zooKeeper)
+    .catch(error => error)
+  },
+  editZooKeeper: function(id, zooKeeperEdits) {
+    return this.where({id})
+    .save(zooKeeperEdits, {method: update})
+    .then(zooKeeper => zooKeeper)
+    .catch(error => error)
+  }
+
+})
